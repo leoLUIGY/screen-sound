148ff66ee26f911ad3840ea6dad7c13d11c5825c
leoLUIGY <luigyleonidas@gmail.com>
2025-08-14 14:10:24 -0300

    initial structure

---
+++ ScreenSound/Migrations/20250814142525_RelacionarArtistaMusica.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelacionarArtistaMusica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ArtistaId",
+                table: "musicas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_musicas_ArtistaId",
+                table: "musicas",
+                column: "ArtistaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_musicas_Artistas_ArtistaId",
+                table: "musicas",
+                column: "ArtistaId",
+                principalTable: "Artistas",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_musicas_Artistas_ArtistaId",
+                table: "musicas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_musicas_ArtistaId",
+                table: "musicas");
+
+            migrationBuilder.DropColumn(
+                name: "ArtistaId",
+                table: "musicas");
+        }
+    }
+}
---
+++ ScreenSound/Migrations/20250814142850_AtualizarReferencias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizarReferencias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+            migrationBuilder.Sql("UPDATE musicas SET ArtistaId = 1 WHERE Id = 1");
+            migrationBuilder.Sql("UPDATE musicas SET ArtistaId = 1 WHERE Id = 2");
+            migrationBuilder.Sql("UPDATE musicas SET ArtistaId = 1 WHERE Id = 3");
+            migrationBuilder.Sql("UPDATE musicas SET ArtistaId = 1 WHERE Id = 4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
